6d4f414cba9bf11d9dc8fec9f6a1f02ec6d24f8f
Daniel Ramos <danielramosacosta1@gmail.com>
2023-11-04 15:34:39 +0100

    green: kill a lonely celll

--- src/Cell.spec.ts
+++ src/Cell.spec.ts
@@ -2,11 +2,37 @@ import { describe, it, expect } from "vitest"
 import { Cell } from "./Cell.js"
 
 describe("Cell", () => {
-  it("compares an alive and dead cell", () => {
+  it("revives a dead cell", () => {
     const dead = Cell.dead()
 
-    const alive = dead.nextGeneration([Cell.alive(), Cell.alive(), Cell.alive()])
+    const alive = dead.nextGeneration([
+      Cell.alive(),
+      Cell.alive(),
+      Cell.alive(),
+      Cell.dead(),
+      Cell.dead(),
+      Cell.dead(),
+      Cell.dead(),
+      Cell.dead(),
+    ])
 
     expect(alive.isAlive()).toBe(true)
+  })
+
+  it("kill a lonely cell", () => {
+    const alive = Cell.alive()
+
+    const dead = alive.nextGeneration([
+      Cell.dead(),
+      Cell.dead(),
+      Cell.dead(),
+      Cell.dead(),
+      Cell.dead(),
+      Cell.dead(),
+      Cell.dead(),
+      Cell.dead(),
+    ])
+
+    expect(dead.isAlive()).toBe(false)
   })
 })
--- src/Cell.ts
+++ src/Cell.ts
@@ -1,17 +1,33 @@
-export class Cell {
+export abstract class Cell {
   static dead(): Cell {
-    return new Cell()
+    return new DeadCell()
   }
 
   static alive() {
-    return new Cell()
+    return new AliveCell()
+  }
+
+  abstract nextGeneration(neighbors: Cell[]): Cell
+
+  abstract isAlive(): boolean
+}
+
+class AliveCell extends Cell {
+  isAlive(): boolean {
+    return true
   }
 
   nextGeneration(neighbors: Cell[]): Cell {
-    return new Cell()
+    return new DeadCell()
+  }
+}
+
+class DeadCell extends Cell {
+  isAlive(): boolean {
+    return false
   }
 
-  isAlive() {
-    return true
+  nextGeneration(neighbors: Cell[]): Cell {
+    return new AliveCell()
   }
 }
