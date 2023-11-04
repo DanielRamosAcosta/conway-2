6a4efcfe0e901d64ae5e5960bb19a7ac5761eded
Daniel Ramos <danielramosacosta1@gmail.com>
2023-11-04 11:50:53 +0100

    green: does nothing if all cells are dead

--- src/Board.spec.ts
+++ src/Board.spec.ts
@@ -1,13 +1,28 @@
 import { it, describe, expect } from "vitest"
 
+class Cell {
+  static dead() {
+    return new Cell()
+  }
+}
+
+class Board {
+  static create(cells: Array<Array<Cell>>): Board {
+    return new Board()
+  }
+
+  nextGeneration(): Board {
+    return this
+  }
+}
 
 describe("Board", () => {
   it("does nothing if all cells are dead", () => {
-    const board: Board = Board.create(
+    const board = Board.create([
       [Cell.dead(), Cell.dead(), Cell.dead()],
       [Cell.dead(), Cell.dead(), Cell.dead()],
-      [Cell.dead(), Cell.dead(), Cell.dead()]
-    )
+      [Cell.dead(), Cell.dead(), Cell.dead()],
+    ])
 
     const nextBoard = board.nextGeneration()
 
