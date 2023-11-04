0881bf6dfe007fdf78b3c8811c1da047ffd84241
Daniel Ramos <danielramosacosta1@gmail.com>
2023-11-04 12:16:15 +0100

    green: board can be compared

--- src/Board.ts
+++ src/Board.ts
@@ -1,8 +1,10 @@
 import { Cell } from "./Cell.js"
 
 export class Board {
+  constructor(private readonly cells: Array<Array<Cell>>) {}
+
   static create(cells: Array<Array<Cell>>): Board {
-    return new Board()
+    return new Board(cells)
   }
 
   nextGeneration(): Board {
